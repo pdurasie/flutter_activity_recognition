8ea26014c782331e26d889f8027c5419c17c6ef4
Patrick Durasiewicz <patrick.durasiewicz@sendit.green>
2023-07-10 11:49:24 +0200

    Adjust expected intent result type

--- android/src/main/kotlin/com/pravera/flutter_activity_recognition/service/ActivityRecognitionIntentReceiver.kt
+++ android/src/main/kotlin/com/pravera/flutter_activity_recognition/service/ActivityRecognitionIntentReceiver.kt
@@ -4,10 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.google.android.gms.location.ActivityRecognitionResult
+import com.google.android.gms.location.ActivityTransitionResult
 
 class ActivityRecognitionIntentReceiver: BroadcastReceiver() {
 	override fun onReceive(context: Context, intent: Intent) {
-		if (ActivityRecognitionResult.hasResult(intent)) {
+		if (ActivityTransitionResult.hasResult(intent)) {
 			intent.setClass(context, ActivityRecognitionIntentService::class.java)
 			ActivityRecognitionIntentService.enqueueWork(context, intent)
 		}
